01a94cef59b57afc99913cdffa1210a15375b4f9
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-06-25 11:44:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
+const clockElement = document.getElementById('clock');
+const timeElement = document.getElementById('time');
+
+// Créer une instance de Hammer.js pour l'élément clock
+const hammer = new Hammer(clockElement);
+
+// Activer la détection des gestes de balayage (swipe)
+hammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+// Détecter le geste de balayage vers le bas
+hammer.on('swipedown', function() {
+  timeElement.style.color = 'red'; // Exemple de modification de la couleur de l'heure
+  timeElement.style.fontSize = '4rem'; // Exemple de modification de la taille de police de l'heure
+});
+
+// Détecter le geste de balayage vers le haut
+hammer.on('swipeup', function() {
+  timeElement.style.color = 'blue'; // Exemple de modification de la couleur de l'heure
+  timeElement.style.fontSize = '8rem'; // Exemple de modification de la taille de police de l'heure
+});
+
 function updateTime() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const timeString = hours + ':' + minutes;
 
-    hours = (hours < 10 ? "0" : "") + hours;
-    minutes = (minutes < 10 ? "0" : "") + minutes;
-    seconds = (seconds < 10 ? "0" : "") + seconds;
-
-    var timeString = hours + ":" + minutes + ":" + seconds;
-    document.getElementById("time").innerText = timeString;
+  timeElement.textContent = timeString;
 }
 
 setInterval(updateTime, 1000);
